f69516446ef3adc191ea32d3d2f6b047621a20cc
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2020-11-14 21:18:14 +0100

    Drawing now works for d3Draw; svg CSS color is not taken;

--- Lahckoko.Blazor/wwwroot/js/d3CircleDisplay.js
+++ Lahckoko.Blazor/wwwroot/js/d3CircleDisplay.js
@@ -1,49 +1,51 @@
-﻿function d3Draw(poly) {
+﻿window.d3DrawFunctionsJS = {
 
-    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    var width = 600 - margin.left - margin.right;
-    var height = 500 - margin.top - margin.bottom;
+    d3Draw: function (poly) {
+        var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+        var width = 600 - margin.left - margin.right;
+        var height = 500 - margin.top - margin.bottom;
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var x = d3.scaleLinear().range([0, 500]);
-    var y = d3.scaleLinear().range([500, 0]);
+        var x = d3.scaleLinear().range([0, 500]);
+        var y = d3.scaleLinear().range([500, 0]);
 
-    x.domain([-1500, 1500]);
-    y.domain([-1500, 1500]);
+        x.domain([-1, 1]);
+        y.domain([-1, 1]);
 
-    //var point = { "x": 24, "y": 31 }
+        //var point = { "x": 24, "y": 31 }
 
-    //var poly = [{ "x": 10, "y": 50 },
-    //{ "x": 20, "y": 20 },
-    //{ "x": 50, "y": 10 },
-    //{ "x": 30, "y": 30 }];
+        //var poly = [{ "x": 10, "y": 50 },
+        //{ "x": 20, "y": 20 },
+        //{ "x": 50, "y": 10 },
+        //{ "x": 30, "y": 30 }];
 
 
-    svg.selectAll("polygon")
-        .data([poly])
-        .enter().append("polygon")
-        .attr("points", function (d) {
-            return d.map(function (d) {
-                return [x(d.x), y(d.y)].join(",");
-            }).join(" ");
-        });
+        svg.selectAll("polygon")
+            .data([poly])
+            .enter()
+            .append("polygon")
+            .attr("points", function (d) {
+                return d.map(function (p) { return [x(p.x), y(p.y)].join(","); }
+                ).join(" ");
+            });
 
-    //svg.append("circle")
-    //    .attr("r", 4)
-    //    .attr("cx", x(point.x))
-    //    .attr("cy", y(point.y))
+        //svg.append("circle")
+        //    .attr("r", 4)
+        //    .attr("cx", x(point.x))
+        //    .attr("cy", y(point.y))
 
-    //// add the X Axis
-    //svg.append("g")
-    //    .attr("transform", "translate(0," + height + ")")
-    //    .call(d3.axisBottom(x));
+        //// add the X Axis
+        //svg.append("g")
+        //    .attr("transform", "translate(0," + height + ")")
+        //    .call(d3.axisBottom(x));
 
-    //// add the Y Axis
-    //svg.append("g")
-    //    .call(d3.axisLeft(y));
-}
+        //// add the Y Axis
+        //svg.append("g")
+        //    .call(d3.axisLeft(y));
+    }
+};
